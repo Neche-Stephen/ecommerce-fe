a9654bfd21d48392486f2d195f4a7a8a37cd6ee8
Chinecherem <chinecheremubawike@gmail.com>
2024-09-12 20:13:08 +0100

    Latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
       </Routes>
 
-      <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+      
 
     </BrowserRouter>
    
