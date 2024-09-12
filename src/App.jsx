75f652b79ebe3794a82317236e6df97135ea622f
Chinecherem <chinecheremubawike@gmail.com>
2024-09-12 15:18:32 +0100

    First version of landing page'

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+
+import LandingPage from './routes/landingPage/LandingPage';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+
+    </BrowserRouter>
+   
+  )
+}
+
+export default App
