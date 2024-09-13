5994b1492e76bd86dbad6217f4425e470194ef65
Chinecherem <chinecheremubawike@gmail.com>
2024-09-13 01:19:08 +0100

    adjusted home path

--- src/App.jsx
+++ src/App.jsx
@@ -26,9 +26,8 @@ function App() {
             <Route path='shop' element={<Shop />} />
             <Route path='auth' element={<Authentication />} />
             <Route path='checkout' element={<Checkout />} />
-          </Route>
-        <Route path="/home" element={<LandingPage />} />
-
+            <Route path="/home" element={<LandingPage />} />
+        </Route>
       </Routes>
 
     </>
