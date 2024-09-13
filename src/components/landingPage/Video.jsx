62ed94d62224460f53bc95260c16f362c6ea94e3
Chinecherem <chinecheremubawike@gmail.com>
2024-09-13 02:00:01 +0100

    removed video import

--- src/components/landingPage/Video.jsx
+++ src/components/landingPage/Video.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import video from '../../assets/landingPage/product.mov'
 
 export default function Video() {
     const fileId = '1ZkqZ3pXPjMa9zZEi3Q-t_fN7T3CvwF1m'; // Replace with your actual file ID
@@ -10,7 +9,6 @@ export default function Video() {
         
         <div className='text-[30px] text-center'>Project Video</div>
         <div className='w-[75%] mx-auto'>
-          {/* <video src={video} loop controls></video> */}
           <iframe src={src} width="100%" height="580" allow="autoplay"></iframe>
           {/* https://drive.google.com/file/d/1ZkqZ3pXPjMa9zZEi3Q-t_fN7T3CvwF1m/view?usp=sharing */}
         </div>
